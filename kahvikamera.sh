2db00d9e4d26fabd3778be8c10c5d1bf1ed52972
Lari Nurkka <lari.nurkka@gmail.com>
2025-05-08 16:43:30 +0300

    Initial commit

---
+++ kahvikamera.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+! command -v ffmpeg \
+    && "This script requires ffmpeg to work!" && exit 1
+! command -v curl \
+    && "This script requires scp to work!" && exit 1
+
+image_path="img.jpg"
+api_key="$(cat apikey.txt)"
+media_source="$(cat mediasource.txt)"
+image_width="1280"
+api_endpoint="https://kattila-api.linkkijkl.fi/coffee/image"
+
+ffmpeg -y -i "$media_source" -vf scale="$image_width":-1 "$image_path"
+
+curl -X PUT --header "X-API-Key: $api_key" -F "file=@$image_path" -v "$api_endpoint"
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Kahvikamera.sh
+
+A simple script used to update coffee status images to [kattila-api](https://github.com/linkkijkl/kattila-api) ([kattila.linkkijkl.fi](https://kattila.linkkijkl.fi)).
+
+### Usage:
+Create files `mediasource.txt` and `apikey.txt` with appropriate contents (without newline), and run as often as you like the image updated.
